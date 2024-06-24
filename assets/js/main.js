6820e686f3e3a6e18e2e96af869bab1cbc0eb0c3
Marlon <61248995+bedimcode@users.noreply.github.com>
2024-06-24 08:05:28 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*=============== CARD POPUP JS ===============*/
+
+
+/* Show modal */
+
+
+/* Hide modal */
+
+
+/* Hide modal on background click */
+
+
+/* Don't hide modal on card click (by event propagation) */
+
